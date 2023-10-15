ef918058836e7642e88f74c1896f70beae90f00c
Jinyao Chen <38099452+cjy513203427@users.noreply.github.com>
2023-10-15 16:46:22 +0200

    Custom env

---
+++ 03_custom_env/s4_double_check_env.py
@@ -0,0 +1,13 @@
+from s2_CustomEnv import SnekEnv
+
+env = SnekEnv()
+episodes = 50
+
+for episode in range(episodes):
+    done = False
+    obs = env.reset()
+    while True:  # not done:
+        random_action = env.action_space.sample()
+        print("action", random_action)
+        obs, reward, done, truncated, info = env.step(random_action)
+        print('reward', reward)
